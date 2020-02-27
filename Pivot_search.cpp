1bbbebc29b11f07e65e392c6e5c9d0c0cd2d141a
AbhayaV2001 <53890164+AbhayaV2001@users.noreply.github.com>
2020-02-27 11:55:35 +0530

    Update Pivot_search.cpp

--- Pivot_search.cpp
+++ Pivot_search.cpp
@@ -1,64 +1,55 @@
 #include<algorithm>
 #include<iostream>
 using namespace std;
-int pivot(int[],int,int);
-int bins(int[],int,int,int );
-int pivse(int arr[],int n,int key);
-
-int main ()
+int bsearch(int a[], int l, int h, int key);
+int main()
 {
-  
-	int ar[50], key, n;
-	cout<<"Enter the Size: "<<endl;
+	int a[20],i,j,n,r,key,temp,pos,pivot;
+	cout<<"Enter the number of elements in the array\n";
 	cin>>n;
-	cout<<"Enter Elements : "<<endl;
-	for (int i = 0; i < n; i++)
- 	cin>>ar[i];
-	cout<<"Enter The KEY :"<<endl;
-	cin>>key;	
-	cout<<"Index of the element is :"<<pivse(ar,n,key);
- 	return 0;
+	cout<<"Enter the elements\n";	
+	for(i=0;i<n;i++)
+		cin>>a[i];
+	cout<<"Enter the key element to be searched\n";
+	cin>>key;
+	cout<<"Enter the number of times of rotation or pivot\n";
+	cin>>r;
+	for(i=0;i<r;i++)
+	{
+		temp = a[n-1];
+		for(j=n-2;j>=0;j--)
+			a[j+1] = a[j];
+		a[0] = temp;
+	}
+	cout<<"\nThe rotated array is\n";
+	for(i=0;i<n;i++)
+		cout<<a[i]<<" ";
+	cout<<endl;
+	pivot = r-1;
+	if(key>a[0])
+		pos = bsearch(a,1,pivot,key);
+	else if(key<a[0])
+		pos = bsearch(a,pivot+1,n-1,key);
+	else
+		pos = 0;
+	if(pos!=-1)
+		cout<<"The key element "<<key<<" is present at position "<<pos<<"\n";
+	else
+		cout<<"The key element "<<key<<" is not present\n";
+	return 0;
 }
-int pivse(int arr[], int n, int key)
+int bsearch(int a[], int l, int h, int key)
 {
-
-	int piv = pivot (arr, 0, n - 1);
- 	if (piv == -1)
-    		return bins (arr, 0, n - 1, key);
- 	if (arr[piv] == key)
-  		return piv;
- 	if (arr[0] <= key)
-   		return bins (arr,0,piv-1,key);
- 	else
-   		return bins(arr, piv + 1, n - 1, key);
-}
-
-int pivot (int arr[], int low, int high)
-{
-	if (high < low)
-   		return -1;
- 	if (high == low)
-  		return low;
-  	int mid = (low + high) / 2;
- 	if (mid < high && arr[mid] > arr[mid + 1])
-  		return mid;
- 	if (mid > low && arr[mid] < arr[mid - 1])
-  		return (mid - 1);
-	if (arr[low] >= arr[mid])
-   		return pivot (arr, low, mid - 1);
-  	else
-    		return pivot (arr, mid + 1, high);
-}
-int bins(int arr[], int low, int high, int key)
-{
-
-	if (high < low)
-   		return -1;
-  	int mid = (low + high) / 2;
- 	if (key == arr[mid])
-    		return mid;
-  	if (key > arr[mid])
-   		return bins (arr, (mid + 1), high, key);
- 	else
-   		return bins(arr, low, (mid - 1), key);
+	int mid;
+	if(l<=h)
+	{
+		mid = (l+h)/2;
+		if(a[mid]==key)
+			return mid;
+		else if(a[mid]<key)
+			return bsearch(a,mid+1,h,key);
+		else 
+			return bsearch(a,l,mid-1,key);
+	}
+	return -1;
 }
