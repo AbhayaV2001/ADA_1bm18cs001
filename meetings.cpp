7e02bdfb5995300469843eb2ba4c87b090a061f6
AbhayaV2001 <53890164+AbhayaV2001@users.noreply.github.com>
2020-03-04 23:59:00 +0530

    Update meetings.cpp

--- meetings.cpp
+++ meetings.cpp
@@ -6,7 +6,7 @@ int main()
 	int n,i,p,q;
 	cout<<"Enter the total no of meetings\n";
 	cin>>n;
-	int s[n],f[n],resst[n],resf[n],mnum[n],
+	int s[n],f[n],resst[n],resf[n],mnum[n];
 	cout<<"Enter the start times of the meetings\n";
 	for(i=0;i<n;i++)
 		cin>>s[i];
